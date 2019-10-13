90e170f9b68bd02875599b82f38684719c97cfbd
Satadru Sanyal <ss13718@ic.ac.uk>
2019-10-13 10:13:27 +0100

    Adding all source code

---
+++ src/sticks.c
@@ -0,0 +1,31 @@
+//
+// Created by Satadru on 12/06/19.
+//
+
+#include <ncurses.h>
+#include <unistd.h>
+#include "sticks.h"
+
+
+/*prints a stick to a window at a set position*/
+void display_left_stick(WINDOW *window, int y_position) {
+  for (int i = 0; i < 3; i++) {
+    mvwprintw(window, y_position +i, 1, "|");
+    mvwprintw(window, y_position +i, 2, "|");
+  }
+
+  wrefresh(window);
+
+}
+
+void display_right_stick(WINDOW *window, int y_position) {
+
+  int end_x_position = getmaxx(window) - 1;
+  for (int i = 0; i < 3; i++) {
+    mvwprintw(window, y_position +i, end_x_position -1, "|");
+    mvwprintw(window, y_position +i, end_x_position -2, "|");
+  }
+  wrefresh(window);
+}
+
+
